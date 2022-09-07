d019348a2c1799a3e2a2422bb3a70a299f8f7722
Hyunfor <navel5148@gmail.com>
2022-09-08 00:04:12 +0900

    상수 문제

---
+++ src/Java_Problems/constant.java
@@ -0,0 +1,16 @@
+package Java_Problems;
+
+public class constant {
+
+	public static void main(String[] args) {
+		// 상수 PI를 만들어보세요.
+        
+		final double PI = 3.14159;
+
+		int radius = 5;
+		double result = radius * 2 * PI;
+		System.out.println(result);
+
+	}
+
+}
--- src/Java_Problems/test.java
+++ src/Java_Problems/test.java
@@ -1,6 +1,9 @@
 package Java_Problems;
 
 public class test {
+	
+	int a = 1;
+	
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
