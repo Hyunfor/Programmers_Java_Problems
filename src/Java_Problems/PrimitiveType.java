225a88b8a67c5d8056eecd1091341643bdb60ff7
Hyunfor <navel5148@gmail.com>
2022-09-08 00:13:40 +0900

    기본형 타입

---
+++ src/Java_Problems/PrimitiveType.java
@@ -0,0 +1,31 @@
+package Java_Problems;
+
+public class PrimitiveType {
+
+	public static void main(String[] args) {
+		// 기본형 타입
+		  
+		boolean isFun = false;
+		        
+		char charValue = 'a';
+		        
+		int intValue = 20;
+		        
+		long longValue = 2147483648L; 
+		
+		// long 타입 값에는 소문자 l이나 대문자 L을 붙여야 합니다.
+		long big = 3456789L;  
+		// float 타입 값에는 소문자 f나 대문자 F를 붙여야 합니다.
+		float f = 32.5f;
+
+		System.out.println(isFun);
+		System.out.println(charValue);
+		System.out.println(intValue);
+		System.out.println(longValue);
+		System.out.println(big);
+		System.out.println(f);
+
+
+	}
+
+}
