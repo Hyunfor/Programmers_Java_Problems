d00626ec17a901fddfdcbd7d9baaeddfecd31857
Hyunfor <navel5148@gmail.com>
2022-09-07 22:45:10 +0900

    주석문

---
+++ src/Java_Problems/commentary.java
@@ -0,0 +1,17 @@
+package Java_Problems;
+
+public class commentary {
+
+	public static void main(String[] args) {
+		// 주석문 . 한줄 주석
+		/*여러줄 
+		 * 주석
+		 * 
+		 * */
+		/** 문서화 주석
+		 * */
+		 /* 이 부분을 주석처리 해서 프로그램이 정상적으로 실행되도록 만들어 보세요. */
+        //System.out.println("HelloWorld");
+    }
+}
+
--- src/Java_Problems/test.java
+++ src/Java_Problems/test.java
@@ -3,7 +3,7 @@ package Java_Problems;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		System.out.println("Hello world");
 
 	}
 
