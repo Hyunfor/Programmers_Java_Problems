1331ae5bded13fc822d58b25579cd5a371686dcb
Hyunfor <navel5148@gmail.com>
2022-09-07 23:41:31 +0900

    변수 문제

---
+++ src/Java_Problems/variable.java
@@ -0,0 +1,27 @@
+package Java_Problems;
+
+public class variable {
+
+	public static void main(String[] args) {
+		 // 정수형 변수, number를 선언하고 3을 저장해보세요.
+        
+		int number = 3;
+
+		System.out.println(number);
+		
+		// 정수를 담는 변수, count 선언
+		//int count;
+
+		// 실수를 담는 변수, average 선언
+		//double average;
+		
+		// 정수를 담는 변수, count를 선언하고 여기에 10을 저장합니다.
+		//int count = 10;
+		
+		// 실수를 담는 변수, average를 선언하고 여기에 11.1을 저장합니다.
+		//double average = 11.1;   
+		
+	}
+
+}
+
