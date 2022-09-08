f18daeefc4070199d2deed74ba2497a9adb0624b
Hyunfor <navel5148@gmail.com>
2022-09-08 16:15:35 +0900

    기본형 타입 형변환2

--- src/Java_Problems/Primitive_TypeCasting.java
+++ src/Java_Problems/Primitive_TypeCasting.java
@@ -3,23 +3,26 @@ package Java_Problems;
 public class Primitive_TypeCasting {
 
 	public static void main(String[] args) {
-		 int intValue = 200;
-	     //빈칸에 long타입 변수 longValue를 선언하고 변수를 intValue를 이용해 초기화해보세요.
+		 long longValue = 20;
+	     //이 아래줄에 int형 변수 intValue를 선언하고 
+		 //longValue에 들어있는 값을 담아보세요.
 	        
-		 long longValue = intValue;
-
-	     System.out.println(longValue);
+		 int intValue = (int) longValue;
+	        
+	     System.out.println(intValue);
 	     
 	     /*
-	      * 묵시적 형 변환
-			크기가 작은 타입을 크기가 더 큰 타입으로 바꿀 때는 컴파일러에 
-			별도의 명령을 하지 않아도 됩니다.
+	      * 명시적 형변환
+	      * 크기가 더 큰 타입을 작은 타입으로 바꿀 때는 명시적으로 변환해주어야 한다.
 	      * */
 	     
-	     int x = 50000;
-	     // 형 변환이 묵시적으로 이루어집니다.
-	     long y = x;
+	     long x = 50000;
 
+	     // 묵시적 형 변환을 시도하면 오류가 발생합니다.
+	     int y = x;
+	     // 다음과 같이 명시적으로 형 변환 해주세요.
+	     int y = (int) x;
+	        
 
 	}
 
---
+++ src/Java_Problems/Primitive_TypeCasting2.java
@@ -0,0 +1,26 @@
+package Java_Problems;
+
+public class Primitive_TypeCasting2 {
+
+	public static void main(String[] args) {
+		 int intValue = 200;
+	     //빈칸에 long타입 변수 longValue를 선언하고 변수를 intValue를 이용해 초기화해보세요.
+	        
+		 long longValue = intValue;
+
+	     System.out.println(longValue);
+	     
+	     /*
+	      * 묵시적 형 변환
+			크기가 작은 타입을 크기가 더 큰 타입으로 바꿀 때는 컴파일러에 
+			별도의 명령을 하지 않아도 됩니다.
+	      * */
+	     
+	     int x = 50000;
+	     // 형 변환이 묵시적으로 이루어집니다.
+	     long y = x;
+
+
+	}
+
+}
