5c266758a0e25df7ff659c06d5a284e8d43aff1a
Hyunfor <navel5148@gmail.com>
2022-09-15 15:32:00 +0900

    this 실습

---
+++ src/Java_Problems/Person2.java
@@ -0,0 +1,13 @@
+package Java_Problems;
+
+public class Person2 {
+	String name;
+	int age;
+	
+	
+	public Person2(String name,int age) {
+		 // 매개변수로 받은 name과 age를 각각 name, age 필드에 저장하세요.
+		this.name = name;
+		this.age = age;
+	}
+}
---
+++ src/Java_Problems/PersonExam2.java
@@ -0,0 +1,11 @@
+package Java_Problems;
+//실행을 위한 코드입니다.
+public class PersonExam2 {
+
+	public static void main(String[] args) {
+		//Person클래스에서 int형 변수를 매개변수로 받는 생성자를 호출합니다.
+		 Person2 person2 = new Person2("사람", 25);
+
+	}
+
+}
