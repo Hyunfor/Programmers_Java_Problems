a44db91e36a7da951d91dd85490c07aa34480bc4
Hyunfor <navel5148@gmail.com>
2022-09-15 15:36:40 +0900

    메소드 오버로딩

---
+++ src/Java_Problems/Car4.java
@@ -0,0 +1,11 @@
+package Java_Problems;
+
+class Car4 {
+    void run() {
+        System.out.println("차가 달립니다.");
+        }
+    // 정수 하나를 매개변수로 받는 메소드, run을 추가해 보세요.
+   public void run(int x) {
+        	
+        }
+    }
---
+++ src/Java_Problems/CarExam4.java
@@ -0,0 +1,13 @@
+package Java_Problems;
+
+//실행을 위한 코드입니다.
+public class CarExam4 {
+ public static void main(String[] args) {
+     // Person클래스에서 String과 int를 매개변수로 받는 생성자를 호출합니다.
+     Car4 car4 = new Car4();
+     
+     car4.run();
+     // int형 매개변수를 받는 run을 호출합니다.
+     car4.run(100);
+ }
+}
